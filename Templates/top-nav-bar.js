18aeaec6d08aa7803f30dd770b3a97bd0451cf05
Travis <60670527+bin0al@users.noreply.github.com>
2022-06-27 13:33:47 -0400

    Basic vue testing

---
+++ Templates/top-nav-bar.js
@@ -0,0 +1,30 @@
+Vue.component('top-nav-bar', {
+    template: `
+    <div>
+        <div class="jumbotron text-center text-white" style="margin-bottom:0;
+            background-image: url('Images/TechBanner.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;">
+            <h1>K&S Ventures Tools</h1>
+        </div>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <a class="navbar-brand" href="index.html">K&S Ventures Tools</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="thermistortest.html">Thermistor Testing</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="vavflow.html">VAV Flow Calculator</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+    </div>
+    `
+});
+console.log("Nav Bar Component Created!");
