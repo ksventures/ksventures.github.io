3b0b70cd2464f84385674b17491be14b3d18baa1
Travis <60670527+bin0al@users.noreply.github.com>
2022-06-29 10:20:08 -0400

    Basic controller data filtering done

--- ControllerIO/javascript.js
+++ ControllerIO/javascript.js
@@ -32,5 +32,42 @@ function updateFilterSettings() {
     filterSettings.controllerIO.DOCount = document.getElementById("doCount").value;
     filterSettings.controllerIO.IntegralActuator = document.getElementById("integralActuator").checked;
     filterSettings.controllerIO.NetStatCount = document.getElementById("netStatCount").value;
-    console.log(filterSettings);
+    console.log(filterControllers(controllerData, filterSettings));
 };
+
+function singleControllerCheck(controllerObject, filterObject) {
+    var brandMatch = false;
+    if (filterObject.controllerBrand.JCI && (controllerObject.Manufacturer == "JCI")) {brandMatch = true};
+    if (filterObject.controllerBrand.Distech && (controllerObject.Manufacturer == "Distech")) {brandMatch = true};
+    var commMatch = false;
+    if (filterObject.controllerComm.BacnetIP && (controllerObject.CommType.includes("BacnetIP"))) {commMatch = true};
+    if (filterObject.controllerComm.BacnetMSTP && (controllerObject.CommType.includes("BacnetMSTP"))) {commMatch = true};
+    if (filterObject.controllerComm.Lon && (controllerObject.CommType.includes("Lon"))) {commMatch = true};
+    if (filterObject.controllerComm.N2 && (controllerObject.CommType.includes("N2"))) {commMatch = true};
+    var ioMatch = false;
+    var workingAI = filterObject.controllerIO.AICount;
+    var workingDI = filterObject.controllerIO.DICount;
+    var workingAO = filterObject.controllerIO.AOCount;
+    var workingDO = filterObject.controllerIO.DOCount;
+    workingAI = workingAI - controllerObject.IO.AICount;
+    workingDI = workingDI - controllerObject.IO.DICount;
+    workingAO = workingAO - controllerObject.IO.AOCount;
+    workingDO = workingDO - controllerObject.IO.DOCount;
+    if (workingAI < 0) {workingAI = 0};
+    if (workingDI < 0) {workingDI = 0};
+    if (workingAO < 0) {workingAO = 0};
+    if (workingDO < 0) {workingDO = 0};
+    if (((workingAI + workingDI) <= controllerObject.IO.UICount) && ((workingAO + workingDO) <= controllerObject.IO.UOCount)) {ioMatch = true};
+    console.log("Controller: " + controllerObject.Model + "   Brand Match: ", brandMatch, "    Comm Match: ", commMatch, "    ioMatch: ", ioMatch);
+    if (brandMatch && commMatch && ioMatch) {return true} else {return false};
+};
+
+function filterControllers (controllerObject, filterObject) {
+    var matchingControllers = [];
+    for (i=0; i<controllerObject.length; i++) {
+        if (singleControllerCheck(controllerObject[i], filterObject)) {
+            matchingControllers.push(controllerObject[i])
+        };
+    };
+    return matchingControllers;
+}
